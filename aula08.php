63ec8ce134464e9ddab2ee065bc2c8d1e593343c
Gislaine Souza <167459605+souuzagi@users.noreply.github.com>
2024-09-10 16:44:44 -0300

    Add files via upload

---
+++ aula08.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href= "Receba-aula08.php?nome=Gislaine&sobrenome=Souza"> Enviar o nome Gislaine Souza</a>
+
+    <form action = "receba-aula08.php" method="post">
+
+        <label for = "nome"> Nome: </label><br>
+        <input tye = "text" name = "nome"><br>
+        <label for = "sobrenome"> Sobrenome: </label><br>
+        <input type = "text" name = "sobrenome"><br>
+        <input type = "submit" value = "Enviar">
+    </form>
