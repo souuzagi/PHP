cce68b37a3a5c403860f0da4a6219ab69deec548
Gislaine Souza <167459605+souuzagi@users.noreply.github.com>
2024-09-10 14:09:20 -0300

    Add files via upload

---
+++ aula10.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<from action=" recebe-aula10.php" method="post">
+    <label for="nome completo">Nome Completo: </label><br>
+    <input type="text" name="nome Completo"><br>
+    <label for="Data de Nascimento">Data de Nascimento: </label><br>
+    <input type="text" name="Data de Nascimento"><br>
+    <label for="Sexo">Sexo: </label><br>
+    <input type="text" name="Sexo"><br>
+    <label for="Estado Civil">Estado Civil: </label><br>
+    <input type="text" name="Estado Civil"><br>
+    <label for="Nacionalidade">Nacionalidade: </label><br>
+    <input type="text" name="Nacionalidade"><br>
+    <label for="CPF">CPF: </label><br>
+    <input type="text" name="CPF"><br>
+    <label for="Endereço Completo">Endereço Completo : </label><br>
+    <input type="text" name="Endereço Completo"><br>
+    <label for="Logradouro">Logradouro: </label><br>
+    <input type="text" name="Logradouro"><br>
+    <label for="Número">Número: </label><br>
+    <input type="text" name="Número"><br>
+    <label for="Complemento">Complemento: </label><br>
+    <input type="text" name="Complemento"><br>
+    <label for="Bairro">Bairro: </label><br>
+    <input type="text" name="Bairro"><br>
+    <label for="Cidade">Cidade: </label><br>
+    <input type="text" name="Cidade"><br>
+    <label for="Estado">Estado: </label><br>
+    <input type="text" name="Estado"><br>
+    <label for="CEP">CEP: </label><br>
+    <input type="text" name="CEP"><br>
+    <label for="Telefone de Contato">Telefone de Contato: </label><br>
+    <input type="text" name="Telefone de Contato"><br>
+    <label for="E-mail">E-mail: </label><br>
+    <input type="text" name="E-mail"><br>
+    <input type="submit" value="Enviar">
+</body>
+</html>  
+</body>
+</html>
